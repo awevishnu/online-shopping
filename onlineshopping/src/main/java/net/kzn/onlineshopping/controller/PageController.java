6db7cf2cf215ca8f2078d28421637828cbbf87c5
Vishnu P <awevishnu@gmail.com>
2017-08-26 00:32:30 +0530

    Completed interface theme and solved other issues

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -11,7 +11,8 @@ public class PageController {
 	@RequestMapping(value={"/","/home","/index"})
 	public ModelAndView index(){
 		ModelAndView mv=new ModelAndView("page");
-		mv.addObject("greeting","Welcome to Spring MVC");
+		mv.addObject("title","Home");
+		mv.addObject("userClickHome",true);
 		return mv;
 	}
 	/*
@@ -25,14 +26,19 @@ public class PageController {
 		return mv;
 	}
 	*/
-	@RequestMapping(value="/vish/{greeting}")
-public ModelAndView test(@PathVariable("greeting")String greeting){
-	if(greeting==null){
-		greeting="Hello there";
-	}
+	@RequestMapping(value="/about")
+public ModelAndView about(){
+	
 	ModelAndView mv=new ModelAndView("page");
-	mv.addObject("greeting",greeting);
+	mv.addObject("title","About Us");
+	mv.addObject("userClickAbout",true);
 	return mv;
 }
-
+	@RequestMapping(value="/contact")
+public ModelAndView contact(){
+	ModelAndView mv=new ModelAndView("page");
+	mv.addObject("title","Contact Us");
+	mv.addObject("userClickContact",true);
+	return mv;
+}
 }
